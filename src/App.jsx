aa1343ce614b79988d37aad5132629b4e7541f38
renaissancejlc <91338859+renaissancejlc@users.noreply.github.com>
2025-08-15 10:57:57 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,31 +5,10 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { CartProvider } from './context/CartContext';
 
 import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
 import PrivacyPolicy from './pages/PrivacyPolicy';
-import TermsOfUse from './pages/TermsOfUse';
-import ShippingAndReturns from './pages/ShippingAndReturns';
-import Press from './pages/Press';
-import Faq from './pages/Faq';
-import Cart from './pages/Cart';
-import Checkout from './pages/Checkout';
-import Confirmation from './pages/Confirmation';
 
 import { useLocation, Navigate } from 'react-router-dom';
 
-function ProtectedConfirmation() {
-  const location = useLocation();
-  const params = new URLSearchParams(location.search);
-  const sessionId = params.get('session_id');
-  const orderHash = params.get('order');
-
-  if (!sessionId && !orderHash) {
-    return <Navigate to="/" replace />;
-  }
-
-  return <Confirmation />;
-}
 
 export default function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
